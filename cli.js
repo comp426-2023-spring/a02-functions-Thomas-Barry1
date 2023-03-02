e8b0686aeb4516da07eb21418318f753cde56b8a
thomaslb <lagaba@live.unc.edu>
2023-03-01 21:03:15 -0500

    Some new changes.

--- cli.js
+++ cli.js
@@ -5,3 +5,14 @@ import fetch from "node-fetch";
 import process from 'process';
 const timezone = moment.tz.guess()
 var input = minimist(process.argv.slice(2))
+if (input.h){
+    let message = 'Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE' +
+    '-h            Show this help message and exit.' + 
+    '-n, -s        Latitude: N positive; S negative.' +
+    '-e, -w        Longitude: E positive; W negative.' +
+    '-z            Time zone: uses tz.guess() from moment-timezone by default.' +
+    '-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.' +
+    '-j            Echo pretty JSON from open-meteo API and exit.';
+    console.log(message);
+    process.exit(0);
+}
