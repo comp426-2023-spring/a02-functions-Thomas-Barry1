670bdb2e83d7f83ea05740dd1e6b9958397942a3
thomaslb <lagaba@live.unc.edu>
2023-03-01 21:02:07 -0500

    Made changes to some files.

--- cli.js
+++ cli.js
@@ -1,1 +1,7 @@
 #!/usr/bin/env node
+import moment from "moment-timezone";
+import minimist from "minimist";
+import fetch from "node-fetch";
+import process from 'process';
+const timezone = moment.tz.guess()
+var input = minimist(process.argv.slice(2))
